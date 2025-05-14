31d519386047518b1c2693f30aec2b28bc1020f7
neo-17 <pmayhs17@gmail.com>
2025-05-15 02:43:56 +0530

    VITE issue

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,7 +1,8 @@
 const nodeEnv = import.meta.env.MODE || 'development';
 
+console.log(import.meta.env.VITE_DEV_APP_URL);
 export const config = {
     apiUrl: nodeEnv === 'development'
-        ? import.meta.env.DEV_APP_URL
-        : import.meta.env.PROD_APP_URL
+        ? import.meta.env.VITE_DEV_APP_URL
+        : import.meta.env.VITE_PROD_APP_URL
 };
